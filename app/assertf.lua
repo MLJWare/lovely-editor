c5038290da0e0b11115b1de48f46918aee215e0b
Mikkel Lykke Jørgensen <mlykkej@gmail.com>
2019-09-22 13:32:16 +0200

    Minor change to assertf util function

--- app/assertf.lua
+++ app/assertf.lua
@@ -1,5 +1,4 @@
-return function (condition, error_msg, ...)
-  if not condition then
-    error(error_msg:format(...), 2)
-  end
+return function (condition, error_msg, a, b, c, d)
+  if condition then return end
+  error(error_msg:format(a, b, c, d), 2)
 end
